5416bef428a256bd085f1e4f791853fc3f0e03d3
Valeria Guzman <valeria.guzman0509@gmail.com>
2019-09-25 20:36:39 -0500

    start

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const port = 9000
+const handler = function handler () {
+     console.log("El servidor esta escuchando correctamente en el puerto 9000...")
+}
+
+const authRoutes = require("./auth")
+
+app.use('/auth', authRoutes)
+// "/auth" es mi ruta y luego ecesito mi sub ruta de login logout y recover. // 
+app.listen (port, handler)
